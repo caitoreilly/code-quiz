d2b402eee84d054be88956e1166468367ff62c42
Caitlin O'Reilly <coreilly29@gmail.com>
2022-08-22 20:43:49 -0400

    created array with object variables for questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,76 @@
-// access Start Quiz button from HTML
+// create variable to access Start Quiz button
 const startButton = document.querySelector("#start");
 console.log(startButton);
+
+//create variables
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+const questionContainerElement = document.getElementById("question-container");
 
-// create variable objects for each question & possible answers
+// create array of question objects for each question with answers choices & correct answer
 const questions = [
   {
     question: "The condition in an if/else statement is enclosed with ____",
-    answers: [
-      { text: "quotes", correct: false },
-      { text: "curly braces", correct: false },
-      { text: "parentheses", correct: true },
-      { text: "square brackets", correct: false },
-    ],
+    choiceA: "quotes",
+    choiceB: "curly braces",
+    choiceC: "parentheses",
+    choiceD: "square brackets",
+    correctChoice: "C",
+  },
+
+  {
+    question: "String concatenation combines a string with a ______",
+    choiceA: "variable",
+    choiceB: "function",
+    choiceC: "for loop",
+    choiceD: "while loop",
+    correctChoice: "A",
+  },
+
+  {
+    question: "Which is NOT a conditional statement?",
+    choiceA: "if",
+    choiceB: "else",
+    choiceC: "else if",
+    choiceD: "if not",
+    correctChoice: "D",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ________",
+    choiceA: "numbers and strings",
+    choiceB: "other arrays",
+    choiceC: "booleans",
+    choiceD: "all of the above",
+    correctChoice: "D",
+  },
+
+  {
+    question:
+      "In order to execute properly, a function must have an initial expression, a condition, an increment expression, and a ______",
+    choiceA: "while loop",
+    choiceB: "statement",
+    choiceC: "pseudocode",
+    choiceD: "console log",
+    correctChoice: "B",
   },
 ];
-// click start button and activate startQuiz function(). Add event listener at bottom of file.
-// first question pops up + timer begins
 
+// click start button and activate startQuiz function(). Add event listener at bottom of file.
+// when button clicked, info on welcome page will go away, leaving the page blank and ready for the first question.
 function startQuiz() {
   console.log("started");
   const welcomePage = document.getElementById("welcome-page");
-  welcomePage.classList.remove("hide");
+  welcomePage.classList.add("hide");
+  showQuestion();
 }
 
-function showQuestion() {
-  questionContainerElement.remove("welcome-page");
-  const questionContainerElement =
-    document.getElementById("question-container");
-}
-
+// first question pops up + timer begins
+// loop through all questions
 // after user answers question, if correct screen will say "correct" and move onto next question
 // after user answers question, if incorrect screen will say "incorrect", move onto next qusestion, AND 10 seconds will subtract from timer
+function showQuestion() {
+  for (var i = 0; i < questions.length; i++) {}
+}
 
 // when all questions are answered OR timer reaches 0, the game will end
 
@@ -44,5 +82,3 @@ function showQuestion() {
 
 // Add event listener to activate Start Quiz button (startButton) by going into the startQuiz function
 startButton.addEventListener("click", startQuiz);
-
-startQuiz();
